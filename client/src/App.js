835fad8940f07c821624ba82ae68754b647b25b1
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-04-20 15:55:17 -0400

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import {Route, Switch, Link} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from "./Movies/Movie"
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -11,8 +13,12 @@ const App = () => {
 
   return (
     <div>
+    {/* HERE WE ARE IMPLEMTNING COMPONENTS EXPORTED FROM THEIR FILE */}
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/"> <MovieList /></Route>
+    {/* HERE IS A DYNAMIC ID URL BY :ID */}
+      <Route path="/movies/:id"> <Movie /></Route>
+
     </div>
   );
 };
